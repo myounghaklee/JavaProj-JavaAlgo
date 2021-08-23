1a92008981a774bb38ef67eca3c2fedf73d4d380
DESKTOP-DNRH5S6\user <leemh9207@ajou.ac.kr>
2021-08-23 14:32:10 +0900

    퇴사-dfs문제풀이완료

--- 완전탐색/boj_14501_퇴사.java
+++ 완전탐색/boj_14501_퇴사.java
@@ -11,7 +11,7 @@ public class boj_14501_Επ»η {
 	private static int[] w;
 	private static int[] v;
 	private static int[] dp;
-
+	private static int ans;
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
@@ -20,13 +20,33 @@ public class boj_14501_Επ»η {
 		v = new int [N+1];
 		dp = new int [N+1];
 		
-		for(int i =1; i<=N ;i++) {
+		for(int i =0; i<N ;i++) {
 			st = new StringTokenizer(br.readLine(), " ");
 			w[i] = Integer.parseInt(st.nextToken());
 			v[i] = Integer.parseInt(st.nextToken());
 		}
-		for(int i=0; i<N; i++) {
-			int chk = w[i] +i;
+		//System.out.println(v[N]);
+		ans = 0;
+		dfs(0,0);
+		
+		System.out.println(ans);
+	}
+
+	private static void dfs(int idx, int a) {
+		if(idx>=N) {
+			
+			ans = Math.max(ans, a);
+			return;
 		}
+		
+		if((idx+w[idx]) <=N ) {
+
+			dfs(idx + w[idx], a+v[idx]);
+		}
+		else dfs(idx+ w[idx] , a);
+		
+		dfs(idx+1, a);
+		
+		
 	}
 }
