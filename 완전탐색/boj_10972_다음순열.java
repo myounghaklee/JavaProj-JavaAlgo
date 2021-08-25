206a65103895f668c9433fb71d0b2dca19230782
myounghak <leemh9207@ajou.ac.kr>
2021-08-25 23:26:01 +0900

    다음순열문제

--- 완전탐색/boj_10972_다음순열.java
+++ 완전탐색/boj_10972_다음순열.java
@@ -1,4 +1,27 @@
+private static boolean nextPermutation(int[] arr) {
+    int i = arr.length-1;
+    while(i > 0 && arr[i-1] >= arr[i]) i--;
+    if(i <= 0) return false;
 
-public class boj_10972_´ÙÀ½¼ø¿­ {
+    int j = arr.length-1;
+
+    while(arr[j] <= arr[i-1]) j--;
+
+    swap(arr, i-1, j);
+    j = arr.length - 1;
+    while(i < j) {
+        swap(arr, i, j);
+        i++;
+        j--;
+    }
+    return true;
+}
+
+private static void swap(int[] arr, int index1, int index2) {
+    int temp = arr[index1];
+    arr[index1] = arr[index2];
+    arr[index2] = temp;
+}
+public class boj_10972_ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ {
 
 }
