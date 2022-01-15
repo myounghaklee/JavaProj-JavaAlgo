fe8af6654ec4f08c63f78aa39ddfe42506b8382d
myounghak <leemh9207@ajou.ac.kr>
2022-01-16 00:49:30 +0900

    기능개발 add

---
+++ 프로그래머스/기능개발/Solution.java
@@ -0,0 +1,11 @@
+package T2.기능개발;
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+    public int[] solution(int[] progresses, int[] speeds) {
+        int[] answer = {};
+        return answer;
+    }
+}
