fba0d3ac4d1c731d6f4443e6d42fad160a9c2c9a
myounghak <leemh9207@ajou.ac.kr>
2022-01-22 21:23:20 +0900

    이중 for문으로 품 ( 최악) ...테케 70%통과

---
+++ 프로그래머스/전화번호목록/Solution.java
@@ -0,0 +1,27 @@
+package T2.전화번호목록;
+
+class Solution {
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        String [] phone_book= {"12","123","1235","567","88"};
+
+        System.out.println(s.solution(phone_book));
+
+    }
+    public boolean solution(String[] phone_book) {
+        boolean answer = true;
+        for(int i =0; i< phone_book.length; i++){
+            String s = phone_book[i];
+            char[] now = phone_book[i].toCharArray();
+            if(i!= phone_book.length-1) {
+                for (int j = i + 1; j < phone_book.length; j++){
+                    if(phone_book[j].contains(s)) return false;
+
+                }
+
+            }else break;
+        }
+        return answer;
+    }
+}
