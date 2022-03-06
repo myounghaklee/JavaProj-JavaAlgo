ed8e30dcbec411f8838ae2276e209f8edcb864cc
myounghak <leemh9207@ajou.ac.kr>
2022-03-06 23:38:48 +0900

    no message

---
+++ src/BOJ/boj_10157자리배정.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class boj_10157자리배정 {
+
+	private static int x;
+	private static int y;
+	private static int N;
+	private static int[][] map;
+	private static int cnt;
+	private static int ny;
+	private static int nx;
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		x = Integer.parseInt(st.nextToken());
+		y = Integer.parseInt(st.nextToken());
+		
+		N = Integer.parseInt(br.readLine());
+		
+		map = new int [y][x];
+		
+		ny= y-1;
+		nx = 0;
+		cnt = 1;
+		while(true) {
+			if(map[ny-1][x]==0)
+		}
+
+	}
+
+}
