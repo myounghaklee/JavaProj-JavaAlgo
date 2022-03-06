ef55838b6984250c8e50f9fe0660353f3d848081
myounghak <leemh9207@ajou.ac.kr>
2022-03-06 23:39:16 +0900

    package add

--- src/BOJ/boj_10157자리배정.java
+++ src/BOJ/boj_10157자리배정.java
@@ -1,3 +1,5 @@
+package BOJ;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
--- src/BOJ/boj_10158개미.java
+++ src/BOJ/boj_10158개미.java
@@ -1,3 +1,5 @@
+package BOJ;
+
 import java.util.*;
 import java.io.*;
 
