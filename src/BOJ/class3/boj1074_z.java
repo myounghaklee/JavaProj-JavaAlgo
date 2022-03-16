03a18eb4972f7591e0b4ed70bca8dc9cbe99ef66
myounghak <leemh9207@ajou.ac.kr>
2022-03-17 01:05:08 +0900

    분할정복 풀이중

---
+++ src/BOJ/class3/boj1074_z.java
@@ -0,0 +1,19 @@
+package BOJ.class3;
+
+import java.util.Scanner;
+
+public class boj1074_z {
+	private static int N;
+	private static int Y;
+	private static int X;
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		Y = sc.nextInt();
+		X = sc.nextInt();
+		
+		leng = Math.pow(2, N);
+	
+	}
+}
