727dd2c8b6a9461f38d16a3dd808614b2ae223df
myounghak <leemh9207@ajou.ac.kr>
2022-03-18 01:33:56 +0900

    분할정복 풀이중

--- src/BOJ/class3/boj1074_z.java
+++ src/BOJ/class3/boj1074_z.java
@@ -6,6 +6,9 @@ public class boj1074_z {
 	private static int N;
 	private static int Y;
 	private static int X;
+	private static double leng;
+	private static int[][] map;
+	private static int cnt;
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
@@ -14,6 +17,16 @@ public class boj1074_z {
 		X = sc.nextInt();
 		
 		leng = Math.pow(2, N);
+		map = new int[(int) leng][(int) leng];
+		cnt = 0;
+		divideNConquere(leng, Y,X);
 	
+	}
+
+	private static void divideNConquere(double range, int y, int x) {
+		int half = (int)(range/2);
+		if(y<half && x<half) {
+			divideNConquere(half, y, x);
+		}else if()
 	}
 }
---
+++ src/goorm/문제3b_페인트.java
@@ -0,0 +1,5 @@
+package goorm;
+
+public class 문제3b_페인트 {
+
+}
