a18b89a6154049a3ae0d4f05be7c3bf53cfc1260
myounghak <leemh9207@ajou.ac.kr>
2022-04-24 01:12:30 +0900

    no message

--- src/TopCoder/전체탐색/암호.java
+++ src/TopCoder/전체탐색/암호.java
@@ -2,9 +2,31 @@ package TopCoder.전체탐색;
 
 public class 암호 {
 
+	/*
+	 * +1하는 숫자를 하나 정한다. 
+	 * 모든 곱을 계산하고 최댓값을 찾는다.
+	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int numbers[] = {1,2,3};
+		long answer = encrypt1(numbers);
+		System.out.println(answer);
 
+	}
+
+	private static long encrypt1(int[] numbers) {
+		long ans =0;
+		for(int i = 0 ;i<numbers.length; i++) {
+			long tmp =1;
+			for(int j =0; j<numbers.length; j++) {
+				if(i==j) {
+					tmp*=(numbers[j]+1);
+				}else {
+					tmp*=(numbers[j]);
+				}
+			}
+			ans = Math.max(ans, tmp);
+		}
+		return ans;
 	}
 
 }
